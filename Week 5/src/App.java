a9a4c8bddb06534e649976435c319cad4f58f796
ajliam <andrewjordan35@gmail.com>
2020-09-19 20:30:06 -0700

    Final submission
    updated with code and put finishing touches

---
+++ Week 5/src/App.java
@@ -0,0 +1,25 @@
+/*
+9.	Create a class named App that has a main method.
+10.	In this class instantiate an instance of each of your logger classes that implement the Logger interface.
+11.	Test both methods on both instances, passing in Strings of your choice.
+*/
+
+
+public class App {
+
+    public static void main(String[] args) {
+
+
+        Logger asteriskLogger = new AsteriskLogger();
+        Logger spacedLogger = new SpacedLogger();
+
+        asteriskLogger.Log("Hi!");
+        asteriskLogger.Error("Bye!");
+
+        System.out.println();
+
+        spacedLogger.Log("Hello");
+        spacedLogger.Error("DANGER!");
+
+    }
+}
---
+++ Week 5/src/SpacedLogger.java
@@ -0,0 +1,27 @@
+/*
+3.	Create two classes that implement the Logger interface
+        a.	AsteriskLogger
+        b.	SpacedLogger
+6.	The SpacedLogger should add spaces between each character of the String argument passed into its methods.
+7.	If the log method received “Hello” as an argument, it should print H e l l o
+8.	The error method should do the same, but with “ERROR:” preceding the spaced out input (i.e. ERROR: H e l l o)
+*/
+
+public class SpacedLogger implements Logger {
+
+
+    @Override
+    public void Log(String str) {
+        StringBuilder log = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            log.append(str.charAt(i)).append(" ");
+        }
+        System.out.println(log);
+    }
+
+    @Override
+    public void Error(String str) {
+        String er = ("ERROR: " + str);
+        Log(er);
+    }
+}
